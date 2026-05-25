e38312446b6b56acc498ce49017609e7c41b780d
anirudhcheeti-dev <anirudhcheeti.dev@gmail.com>
2026-05-25 23:29:39 +0530

    Time: 100 ms (11.84%) | Memory: 81.7 MB (83.34%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,7 +1,13 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < nums.length; i++){
+            pq.add(nums[i]);
+        }
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            result[i] = pq.poll();
+        }
+        return result;
     }
-
 }
