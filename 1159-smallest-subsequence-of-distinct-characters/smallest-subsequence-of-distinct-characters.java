932a7b4a61a43db81d409b8188eaad721592bb94
anirudhcheeti-dev <anirudhcheeti.dev@gmail.com>
2026-07-19 23:45:48 +0530

    Time: 4 ms (25.77%) | Memory: 42.7 MB (92.89%) - LeetSync

--- 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -7,27 +7,27 @@ class Solution {
         }
         boolean[] visit=new boolean[26];
         for(int i=0;i<s.length();i++){
-            freq[s.charAt(i) - 'a']--;
-                if(visit[s.charAt(i)-'a']){
+            char ch=s.charAt(i);
+            freq[ch - 'a']--;
+                if(visit[ch-'a']){
                     continue;
                 }
-                else if(st.isEmpty()||(st.peek()<s.charAt(i))){
-                    st.push(s.charAt(i));
-                    visit[s.charAt(i)-'a']=true;
+                else if(st.isEmpty()||(st.peek()<ch)){
+                    st.push(ch);
+                    visit[ch-'a']=true;
                 }else{
-                    while(!st.isEmpty()&&st.peek()>s.charAt(i)&&freq[st.peek() - 'a']>0){
+                    while(!st.isEmpty()&&st.peek()>ch &&freq[st.peek() - 'a']>0){
                         char popped = st.pop(); 
                         visit[popped - 'a'] = false;
                     }
-                    st.push(s.charAt(i));
-                    visit[s.charAt(i) - 'a'] = true;
+                    st.push(ch);
+                    visit[ch- 'a'] = true;
                 }
             }
         StringBuilder sb=new StringBuilder();
         while(!st.isEmpty()){
             sb.append(st.pop());
         }
-        sb.reverse();
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
