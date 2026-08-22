e67018df4ba3ad5473fceb58882ff080483a33f7
anirudhcheeti-dev <anirudhcheeti.dev@gmail.com>
2026-08-22 18:52:40 +0530

    Time: 657 ms (83.62%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select s.user_id,round(ifnull (avg(action='confirmed'),0),2) as confirmation_rate
+select s.user_id,round(ifnull (avg(c.action='confirmed'),0),2) as confirmation_rate
 # if null is used to consider the null values as 0 
 #here avg directly checks for any values which are confirmenf and also for timeout values and does avg
 from Signups as s
 left join Confirmations as c on s.user_id=c.user_id
-group by user_id;
+group by s.user_id;
