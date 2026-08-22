710e6ffe0fee47a47aeeb14ca4ebee0359f0d130
anirudhcheeti-dev <anirudhcheeti.dev@gmail.com>
2026-08-22 17:05:39 +0530

    Time: 382 ms (49.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select e1.name from Employee as e1
+join Employee e2 on e1.id=e2.managerId 
+group by e1.id
+# we need to use having after group by for aggregate functions 
+having count(e2.managerId)>=5;
