e6989cd25c14b530c11513aeb7b58c9d897c34d8
anirudhcheeti-dev <anirudhcheeti.dev@gmail.com>
2026-09-06 22:06:06 +0530

    Time: 1 ms (86.77%) | Memory: 44.2 MB (38.61%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        helper(0,nums,res,new ArrayList<>());
-        return res;
+        List<List<Integer>> list=new ArrayList<>();//Global List
+        helper(0,nums,list,new ArrayList<>());
+        return list;
     }
-    private static void helper(int index, int[] nums, List<List<Integer>> res, List<Integer> curr) {
-        if(index==nums.length){
-            res.add(new ArrayList<>(curr));
-            return;
+    public void helper(int st,int[] nums, List<List<Integer>> res , List<Integer> curr){
+
+        res.add(new ArrayList<>(curr));
+        //base case records every subset
+        
+        for(int i=st;i<nums.length;i++){
+            curr.add(nums[i]);
+            helper(i+1,nums,res,curr);//here when we do i+1 we dont actually change the for loop working but declare a new i
+            curr.remove(curr.size()-1);
         }
-        curr.add(nums[index]);
-        helper(index+1,nums,res,curr);
-        curr.remove(curr.size()-1);
-        helper(index+1,nums,res,curr);
-    }
+    }//good quetion
 }
